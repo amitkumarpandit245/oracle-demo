1ffdf5e3429aacd54fafdc3c37b651a233217a45
Amit Kumar <amit.kumar75@ibm.com>
2022-05-07 18:03:48 +0530

    Adding Swagger UI

--- todo-list-api/src/main/java/com/oracle/demo/TodoListApiApplication.java
+++ todo-list-api/src/main/java/com/oracle/demo/TodoListApiApplication.java
@@ -3,12 +3,15 @@ package com.oracle.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * Main method to start spring boot application
  * 
  * @author Amit Kumar
  */
 @SpringBootApplication
+@EnableSwagger2
 public class TodoListApiApplication {
 /**
  * Main method for spring boot application
